72d4626f5cea79bfc1a36afd427e8b2c452eb915
dragondreams <catsrule2007@gmail.com>
2015-06-19 10:33:40 -0700

    adding submenu to the primary sidebar

--- archive.php
+++ archive.php
@@ -5,6 +5,13 @@
 <div id="container">
 			<div id="sidebar"> 
 				<?php dynamic_sidebar( 'primary' ); ?> 
+				
+				<div id="submenu">
+				<ul>
+					<?php if ($post->post_parent) { wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => __(''))); 
+					} else { wp_list_pages(array('child_of' => $post->ID, 'title_li' => __('')));} ?>
+				</ul>
+			</div>
 			</div>
                 
 <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
