2fc6ac39fc628ed6ca095a478fe7b6c81e9c50de
dragondreams <catsrule2007@gmail.com>
2015-06-19 12:32:49 -0700

    changes to header.php

--- header.php
+++ header.php
@@ -3,7 +3,44 @@
 <head>
     <meta charset="utf-8">
     <title><?php bloginfo('name') ?></title>
-<!-- Begin Meta -->
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<?php if(is_404() || is_search() || is_archive()) { ?>
+	<meta name="googlebot" content="noindex,noarchive,follow,noodp" />
+    <meta name="robots" content="noindex,follow" />
+    <meta name="msnbot" content="noindex,follow" />
+<?php }?>
+<title>
+
+<?php
+// Titles for different page types
+
+if ( is_home() || is_front_page() ) : // Home or Front Page
+	bloginfo('name');
+	global $page, $paged; // Pagenating - Thanks Automatic for this idea - taken from the TwentyTen theme
+		if ( $paged >= 2 || $page >= 2 ):
+			echo ' | ' . sprintf('Page %s', max( $paged, $page ) );
+		 endif;
+	echo ' | ';
+	bloginfo('description');
+	elseif (is_search()): // For Search Pages
+		echo 'Search Results for: '; the_search_query(); echo ' | '; bloginfo('name');
+	elseif (is_tag()): // For Tag Archives
+		echo 'Tag Archive for: '; single_tag_title();  echo ' | '; bloginfo('name');
+	elseif (is_category()): // For Category Archives
+		echo 'Category Archive for: '; single_cat_title(); echo ' | '; bloginfo('name');
+	elseif (is_day()): // For Day, Month, Year Archives
+		echo 'Archive for: '; the_time('F jS, Y'); echo ' | '; bloginfo('name');
+	elseif (is_month()): // Month, Year Archives
+		echo 'Archive for: '; the_time('F, Y'); echo ' | '; bloginfo('name');
+	elseif (is_year()): // Year Archives
+		echo 'Archive for: '; the_time('Y');   echo ' | '; bloginfo('name');
+	else : // Every other sort of archive
+		wp_title('', TRUE, 'right'); echo ' | '; bloginfo('name');
+	endif;
+?>
+</title>
+
+<!-- Begin Meta
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex, nofollow, noarchive"/>
